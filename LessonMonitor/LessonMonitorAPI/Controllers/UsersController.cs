0bd0e65dbe2dd50ed0c15b87f820417e013ce945
demiddima <demid.dima.1996@gmail.com>
2024-01-29 09:52:01 +1000

    add User and UsersController

---
+++ LessonMonitor/LessonMonitorAPI/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace LessonMonitorAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+            
+        }
+
+        [HttpGet]
+        public User[] Get(string userName)
+        {
+            var rnd = new Random();
+
+            var users = new List<User>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var user = new User();
+
+                user.Name = userName + i;
+                user.Age = rnd.Next(7, 70);
+
+                users.Add(user);
+            }
+            return users.ToArray();
+        }
+    }
+}
