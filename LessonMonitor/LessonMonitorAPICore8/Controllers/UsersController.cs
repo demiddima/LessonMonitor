695ce003c1f1b5a2f4c566e071794a316c1d1649
demiddima <demid.dima.1996@gmail.com>
2024-01-29 12:04:39 +1000

    add project core 8

---
+++ LessonMonitor/LessonMonitorAPICore8/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using LessonMonitorAPICore8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LessonMonitorAPICore8.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+        public UsersController()
+        {
+            
+        }
+
+        [HttpGet]
+        public User[] Get(string userName)
+        {
+            var rnd = new Random();
+
+            var users = new List<User>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var user = new User();
+
+                user.Name = userName + i;
+                user.Age = rnd.Next(7, 70);
+
+                users.Add(user);
+            }
+            return users.ToArray();
+        }
+    }
+}
---
+++ LessonMonitor/LessonMonitorAPICore8/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace LessonMonitorAPICore8.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
